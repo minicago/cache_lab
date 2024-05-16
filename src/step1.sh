58f19dbb07756e874a05090b50d830d1dc374730
minicago <vipbyy@126.com>
2024-05-16 22:30:09 +0800

    finished lab1

--- src/step1.sh
+++ src/step1.sh
@@ -4,9 +4,9 @@ rm ./src/csim  > /dev/null 2<&1
 rm ./src/csim-ref  > /dev/null 2<&1
 rm ./src/test-csim > /dev/null 2<&1
 cd ./src  > /dev/null 2<&1
-gcc -g -Wall -Werror -std=c99 -m64 -o csim csim.c cachelab.c -lm  >/dev/null 2>&1
+gcc -g -Wall -Werror -std=c99 -m64 -o csim csim.c cachelab.c -lm  2>&1
 gcc -g -Wall -Werror -std=c99 -m64 -o csim-ref csim-ref.c cachelab.c -lm
-gcc -g -Wall -Werror -std=c99 -m64 -o test-csim test-csim.c >/dev/null 2>&1
+gcc -g -Wall -Werror -std=c99 -m64 -o test-csim test-csim.c  2>&1
 python driver.py > info 2>&1
 cat info | grep "程序无效" >result 2>&1
 if [ $? -eq 0 ]; then
